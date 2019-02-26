a4f07bee98821a7320908c17c53be346dfb58d75
8bit_shadow <shadowmen2010@hotmail.com>
2019-02-27 03:07:34 +1000

    V1.0.0.0

---
+++ Discord translator/Discord translator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Discord_translator
+{
+    partial class DiscordTranslator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiscordTranslator));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.aboutBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sldBrOpacity = new System.Windows.Forms.TrackBar();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxTranslated = new System.Windows.Forms.TextBox();
+            this.txtBxEnglish = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sldBrOpacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(293, 25);
+            this.toolStrip1.TabIndex = 39;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutBtn});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(45, 22);
+            this.toolStripDropDownButton2.Text = "Help";
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(180, 22);
+            this.aboutBtn.Text = "About";
+            this.aboutBtn.ToolTipText = "About SOL\'s mass renamer form";
+            this.aboutBtn.Click += new System.EventHandler(this.aboutBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Opacity";
+            // 
+            // sldBrOpacity
+            // 
+            this.sldBrOpacity.LargeChange = 25;
+            this.sldBrOpacity.Location = new System.Drawing.Point(237, 57);
+            this.sldBrOpacity.Maximum = 100;
+            this.sldBrOpacity.Minimum = 10;
+            this.sldBrOpacity.Name = "sldBrOpacity";
+            this.sldBrOpacity.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.sldBrOpacity.Size = new System.Drawing.Size(45, 242);
+            this.sldBrOpacity.TabIndex = 37;
+            this.sldBrOpacity.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.sldBrOpacity.Value = 100;
+            this.sldBrOpacity.Scroll += new System.EventHandler(this.sldBrOpacity_Scroll);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Location = new System.Drawing.Point(131, 276);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 36;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(11, 276);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 35;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Translated text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 13);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "English to discord word spam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Normal text";
+            // 
+            // txtBxTranslated
+            // 
+            this.txtBxTranslated.AcceptsReturn = true;
+            this.txtBxTranslated.AcceptsTab = true;
+            this.txtBxTranslated.AllowDrop = true;
+            this.txtBxTranslated.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtBxTranslated.Location = new System.Drawing.Point(12, 184);
+            this.txtBxTranslated.Multiline = true;
+            this.txtBxTranslated.Name = "txtBxTranslated";
+            this.txtBxTranslated.ReadOnly = true;
+            this.txtBxTranslated.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBxTranslated.Size = new System.Drawing.Size(194, 70);
+            this.txtBxTranslated.TabIndex = 30;
+            // 
+            // txtBxEnglish
+            // 
+            this.txtBxEnglish.AcceptsReturn = true;
+            this.txtBxEnglish.AcceptsTab = true;
+            this.txtBxEnglish.AllowDrop = true;
+            this.txtBxEnglish.BackColor = System.Drawing.Color.White;
+            this.txtBxEnglish.ForeColor = System.Drawing.Color.Black;
+            this.txtBxEnglish.Location = new System.Drawing.Point(12, 73);
+            this.txtBxEnglish.Multiline = true;
+            this.txtBxEnglish.Name = "txtBxEnglish";
+            this.txtBxEnglish.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBxEnglish.Size = new System.Drawing.Size(194, 70);
+            this.txtBxEnglish.TabIndex = 31;
+            // 
+            // DiscordTranslator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 316);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sldBrOpacity);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBxTranslated);
+            this.Controls.Add(this.txtBxEnglish);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DiscordTranslator";
+            this.Text = "Discord Translator";
+            this.Load += new System.EventHandler(this.DiscordTranslator_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sldBrOpacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem aboutBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar sldBrOpacity;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxTranslated;
+        private System.Windows.Forms.TextBox txtBxEnglish;
+    }
+}
+
---
+++ Discord translator/Discord translator/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_translator
+{
+    public partial class DiscordTranslator : Form
+    {
+        public DiscordTranslator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            txtBxTranslated.Text = "";
+
+            //globalb initializers
+            string textTranslated = "";
+            string textTranslating = "";
+            string curChar = "";
+            int i = 0;
+            int charI = 0;
+
+            //prep
+            string text = txtBxEnglish.Text;
+            String[] textSeperated = text.Split();
+            int textLength = textSeperated.Length;
+
+            //runtime
+            foreach (var word in textSeperated)
+            {                                                                           //Tforeach word
+                int wordLength = word.Length;                                           //|--get the length of the word
+                i++;                                                                    //|--itterate the identifier to the current word
+                                                                                        //|
+                foreach (char wordChar in word)                                         //|
+                {                                                                       //|-Tforeach character in each word
+                    charI++;                                                            //|-|----itterate the identifier to the current character of the current word
+                    curChar = wordChar.ToString();                                      //|-|----convert the character to a string
+                    textTranslating = curChar.Insert(0, ":regional_indicator_");        //|-|----insert the discord emoticon identifier before the character
+                                                                                        //| |
+                    if (textLength == 1)                                                //| |
+                    {                                                                   //|-|---Tif there is only one word
+                        if (charI != wordLength)                                        //| |   |
+                        {                                                               //|-|---|---Tif the current character is not the last character of the word
+                            textTranslating = textTranslating.Insert(21, ": ");         //|-|---|---|----add a colon and a space so the next character can start
+                        }
+                        else
+                        {                                                        //|-|---|---\c)otherwise if the current character is the last character
+                            textTranslating = textTranslating.Insert(21, ":");          //|-|---|---|----only add colon
+                        }                                                               //|-|---|---|e)
+                    }
+                    else
+                    {                                                            //|-|---\c)otherwise, if there's more than one word
+                        if (i != textLength || charI != wordLength)                     //|-|---|
+                        {                                                               //|-|---|---Tif the current word is not the last word and if the current character is not the last character
+                            textTranslating = textTranslating.Insert(21, ": ");         //|-|---|---|----add a colon and space
+                        }
+                        else
+                        {                                                        //|-|---|---\c)otherwise, if the current character is the last character
+                            textTranslating = textTranslating.Insert(21, ":");          //|-|---|---|only add a colon
+                        }                                                               //|-|---|---|e)
+                    }                                                                   //|-|---|e)
+                    textTranslated += textTranslating;                                  //|-|----append the result to the previous results 
+                                                                                        //| |
+                }                                                                       //|-|L)
+                                                                                        //|
+                if (i != textLength)                                                    //|
+                {                                                                       //|-Tif the current word is not the last word
+                    txtBxTranslated.Text += (textTranslated + " ");                     //|-|----output the resultant word  and add an additional space between words
+                }
+                else
+                {                                                                //|-\c)otherwise if the current word is the last word
+                    txtBxTranslated.Text += textTranslated;                             //|-|----simply output the result of the word
+                }                                                                       //|-|e)
+                textTranslated = "";                                                    //|--reset the variables
+                textTranslating = "";                                                   //|
+                charI = 0;                                                              //|
+            }                                                                           //|L)
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBxTranslated.Text = "";
+            txtBxEnglish.Text = "";
+        }
+
+        private void sldBrOpacity_Scroll(object sender, EventArgs e)
+        {
+            double opacity = sldBrOpacity.Value;        //get the value from the opacity slider
+            double test = (opacity / 100);              //convert to decimal
+
+            this.Opacity = test;                        //set the opacity
+        }
+
+        private void DiscordTranslator_Load(object sender, EventArgs e)
+        {
+            int s;
+
+            if (int.TryParse((Opacity * 100).ToString(), out s))
+            {
+                sldBrOpacity.Value = s;     //set upacity on load
+            }
+        }
+
+        private void aboutBtn_Click(object sender, EventArgs e)
+        {
+            AboutBox test = new AboutBox();
+            Form isOpen = Application.OpenForms["AboutDT"];     //get if the window is still open
+
+            if (isOpen != null) { isOpen.Close(); }             //if it is then close it
+
+            test.Show();
+        }
+    }
+}
